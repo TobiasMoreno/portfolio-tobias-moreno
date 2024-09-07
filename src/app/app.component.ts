644b7c34204b2d81002d5ec1136f3d7f74050c5b
405596-MorenoTobias <405596@tecnicatura.frc.utn.edu.ar>
2024-09-07 12:49:59 -0300

    made inicio, sobre-mi, updates footer,header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './ui/footer/footer.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 import { HeaderComponent } from './ui/header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeaderComponent, ProjectListComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent, ProjectListComponent, InicioComponent, SobreMiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
