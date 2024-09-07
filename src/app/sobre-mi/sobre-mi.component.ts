10139488c184c6b410d50eae0f400c8bd08385c9
405596-MorenoTobias <405596@tecnicatura.frc.utn.edu.ar>
2024-09-07 18:44:12 -0300

    update images and description in the projects.json

--- src/app/sobre-mi/sobre-mi.component.ts
+++ src/app/sobre-mi/sobre-mi.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sobre-mi',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sobre-mi.component.html',
-  styleUrl: './sobre-mi.component.css'
+  styleUrl: './sobre-mi.component.css',
 })
 export class SobreMiComponent {
-
 }
