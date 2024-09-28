8a9d97fb7580639d500ccfce4c12199b05e1d338
405596-MorenoTobias <405596@tecnicatura.frc.utn.edu.ar>
2024-09-28 12:34:16 -0300

    add functionality form

--- src/app/form-contactanos/form-contactanos.component.ts
+++ src/app/form-contactanos/form-contactanos.component.ts
@@ -1,39 +1,62 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { toast } from 'ngx-sonner';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgxSonnerToaster, toast } from 'ngx-sonner';
 import { hasEmailError, isRequired } from './validators';
+import emailjs from '@emailjs/browser';
+import { environment } from '../../environments/enviroment';
 
-export interface SubmitForm{
-  nombre: FormControl<string | null>
-  email: FormControl<string | null>
-  mensaje: FormControl<string | null>
+export interface SubmitForm {
+  nombre: FormControl<string | null>;
+  email: FormControl<string | null>;
+  mensaje: FormControl<string | null>;
+  asuntoEmail: FormControl<string | null>;
 }
 
 @Component({
   selector: 'app-form-contactanos',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgxSonnerToaster],
   templateUrl: './form-contactanos.component.html',
   styleUrl: './form-contactanos.component.css',
 })
 export class FormContactanosComponent {
   private _formBuilder = inject(FormBuilder);
 
-  
   form = this._formBuilder.group<SubmitForm>({
     nombre: this._formBuilder.control('', [Validators.required]),
-    mensaje: this._formBuilder.control('', [
-      Validators.required,
-    ]),
     email: this._formBuilder.control('', [
       Validators.required,
       Validators.email,
     ]),
+    mensaje: this._formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+    asuntoEmail: this._formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   });
 
-  submit() {
-    console.log(this.form.value);
-    
+  async submit() {
+    if (!this.form.valid) {
+      toast.error('Error al Enviar Formulario');
+      return;
+    }
+    emailjs.init(environment.API_KEY_EMAILJS);
+    await emailjs.send('service_vlnpsqq', 'template_ix8q55z', {
+      asuntoEmail: this.form.value.asuntoEmail,
+      nombre: this.form.value.nombre,
+      email: this.form.value.email,
+      mensaje: this.form.value.mensaje,
+    });
+    toast.success('Enviado');
+    this.form.reset();
   }
   isRequired(field: 'email' | 'nombre' | 'mensaje') {
     return isRequired(field, this.form);
@@ -42,5 +65,4 @@ export class FormContactanosComponent {
   hasEmailError() {
     return hasEmailError(this.form);
   }
-
 }
--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    DATA_URL : 'assets/data/projects.json'
+    DATA_URL : 'assets/data/projects.json',
+    API_KEY_EMAILJS: 'ZzoXl_F00uDfIUb29'
 }
